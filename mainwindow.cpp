08506d51e52e4aef9466dd6313c785ab3d0e6a48
RaffayIrfan <78643689+RaffayIrfan@users.noreply.github.com>
2025-12-05 19:56:14 +0500

    made functioning using signal slots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,8 @@
 #include<QLabel>
 #include<QVBoxLayout>
 #include<QHBoxLayout>
+#include<QString>
+
 
 #include<iostream>
 #include<limits>
@@ -14,57 +16,6 @@
 
 using namespace std;
 
-std::mt19937& getRng() {
-    static std::random_device rd;
-    static std::mt19937 rng(rd());
-    return rng;
-}
-
-int randomInt(int low, int high) {
-    std::uniform_int_distribution<int> dist(low, high);
-    return dist(getRng());
-}
-
-class deck{
-    private:
-        vector<int> cards;
-    public:
-        deck(){
-            for(int i=1; i<=50; i++){
-                cards.push_back(i);
-            }
-        }
-
-        int draw_rand_card(){
-            if(cards.empty()){
-                cout << "Deck is empty!\n";
-                return -1;
-            }
-            int rand_card_index = randomInt(0, cards.size()-1);
-            int card = cards[rand_card_index];
-            cards.erase(cards.begin() + rand_card_index);
-            return card;
-        }
-
-        void refill(){
-            cards.clear();
-            for(int i=1; i<=50; i++){
-                cards.push_back(i);
-            }
-        }
-        
-        void print(){
-            cout << endl;
-            for(int card : cards){
-                cout << card << " ";
-            }
-        }
-
-        void discard_card(int card){
-            cards.push_back(card);
-        }
-};
-
 bool checkwin(array<int, 10> tower) {
     for (int i = 0; i < 9; i++) {
         if (tower[i] > tower[i + 1]) {
@@ -74,13 +25,10 @@ bool checkwin(array<int, 10> tower) {
     return true;
 }
 
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setWindowTitle("Tower Blaster GUI Client");
-
-    deck deck;
 
     QWidget *centralWidget = new QWidget(this);
     
@@ -93,33 +41,37 @@ MainWindow::MainWindow(QWidget *parent)
     toptext->setStyleSheet("font-weight: bold; font-size: 16px;");
     master_layout->addWidget(toptext);
 
-    array<QPushButton*,10> buttons1;
-    array<QPushButton*,10> buttons2;
-
     QVBoxLayout *tower1 = new QVBoxLayout(this);
     QVBoxLayout *tower2 = new QVBoxLayout(this);
+    
+    drawn_card = deck.draw_rand_card();
 
-    for (int i = 0; i < 10; ++i) {
-        QPushButton *button1 = new QPushButton(this);
-        button1->setText(to_string(deck.draw_rand_card()).c_str());
-        buttons1[i]=button1;
-        tower1->addWidget(button1);
-
-        QPushButton *button2 = new QPushButton(this);
-        button2->setText(to_string(deck.draw_rand_card()).c_str());
-        buttons2[i]=button2;
-        tower2->addWidget(button2);
-    }
-
-    int drawn_card = deck.draw_rand_card();
-
-    QLabel *drawn_card_label = new QLabel(this);
     drawn_card_label->setText(("Drawn Card: " + to_string(drawn_card)).c_str());
     drawn_card_label->setAlignment(Qt::AlignCenter);
     drawn_card_label->setStyleSheet("font-weight: bold; font-size: 14px;");
 
     QPushButton *newcard = new QPushButton(this);
     newcard->setText("Draw New Card");
+
+    for (int i = 0; i < 10; ++i) {
+        QPushButton *button1 = new QPushButton(this);
+        button1->setText(to_string(deck.draw_rand_card()).c_str());
+        buttons1[i]=button1;
+        tower1->addWidget(button1);
+        button1->setProperty("button_index", i); 
+
+        connect(button1,SIGNAL(clicked()), this, SLOT(On_tower_clicked()));
+
+        QPushButton *button2 = new QPushButton(this);
+        button2->setText(to_string(deck.draw_rand_card()).c_str());
+        buttons2[i]=button2;
+        tower2->addWidget(button2);
+        button2->setProperty("button_index", i);
+
+        connect(button2,SIGNAL(clicked()), this, SLOT(On_tower_clicked()));
+    }
+
+    connect(newcard, SIGNAL(clicked()), this, SLOT(On_draw_clicked()));
 
     towers->addLayout(tower1);
     towers->addLayout(tower2);
@@ -134,3 +86,21 @@ MainWindow::MainWindow(QWidget *parent)
 }
 
 MainWindow::~MainWindow() {}
+
+void MainWindow::On_tower_clicked() {
+    QPushButton *button = qobject_cast<QPushButton*>(sender());
+
+    int button_index = button->property("button_index").toInt();
+     
+    int temp = drawn_card;
+    drawn_card = button->text().toInt();
+    button->setText(QString::number(temp));
+
+    drawn_card_label->setText(("Drawn Card: " + to_string(drawn_card)).c_str());
+}
+
+void MainWindow::On_draw_clicked() {
+    deck.discard_card(drawn_card);
+    drawn_card = deck.draw_rand_card();
+    drawn_card_label->setText(("Drawn Card: " + to_string(drawn_card)).c_str());
+}
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,73 @@
 
 #include <QMainWindow>
 
+#include<QWidget>
+#include<QPushButton>
+#include<QLabel>
+#include<QVBoxLayout>
+#include<QHBoxLayout>
+
+#include<iostream>
+#include<limits>
+#include<random>
+#include<vector>
+#include<array>
+#include<algorithm>
+
+using namespace std;
+
+class deck{
+    private:
+        vector<int> cards;
+    public:
+        deck(){
+            for(int i=1; i<=50; i++){
+                cards.push_back(i);
+            }
+        }
+
+        std::mt19937& getRng() {
+            static std::random_device rd;
+            static std::mt19937 rng(rd());
+            return rng;
+        }
+
+        int randomInt(int low, int high) {
+            std::uniform_int_distribution<int> dist(low, high);
+            return dist(getRng());
+        }
+
+        int draw_rand_card(){
+            if(cards.empty()){
+                cout << "Deck is empty!\n";
+                return -1;
+            }
+            int rand_card_index = randomInt(0, cards.size()-1);
+            int card = cards[rand_card_index];
+            cards.erase(cards.begin() + rand_card_index);
+            return card;
+        }
+
+        void refill(){
+            cards.clear();
+            for(int i=1; i<=50; i++){
+                cards.push_back(i);
+            }
+        }
+        
+        void print(){
+            cout << endl;
+            for(int card : cards){
+                cout << card << " ";
+            }
+        }
+
+        void discard_card(int card){
+            cards.push_back(card);
+        }
+};
+
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -10,6 +77,17 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    deck deck; 
+    int drawn_card; 
+    QLabel* drawn_card_label= new QLabel(this); 
+    
+    std::array<QPushButton*, 10> buttons1;
+    std::array<QPushButton*, 10> buttons2;
+
+public slots:
+    void On_tower_clicked();
+    void On_draw_clicked();
+    //void On_discard_clicked();
 };
 
 #endif // MAINWINDOW_H
