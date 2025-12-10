f1f723bb90acd43ba2c4599e5c4d0b5fb49c2c27
RaffayIrfan <78643689+RaffayIrfan@users.noreply.github.com>
2025-12-10 21:21:20 +0500

    .

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+
 #include<QWidget>
 #include<QPushButton>
 #include<QLabel>
@@ -6,6 +7,7 @@
 #include<QHBoxLayout>
 #include<QString>
 #include<QMessageBox>
+#include<QStackedWidget>
 
 #include<iostream>
 #include<limits>
@@ -18,7 +20,7 @@ using namespace std;
 
 bool checkwin(array<QPushButton *, 10> tower) {
     array<int,10> button_value;
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i <10; i++) {
         button_value[i] = tower[i]->text().toInt();
     }
     for (int i = 0; i < 9; i++) {
@@ -33,47 +35,59 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setWindowTitle("Tower Blaster GUI Client");
-    
+
     main_menu();
     player1v1();
+    with_ai();
 
-    setCentralWidget(mainmenu);
+    stackedWidget->addWidget(mainmenu);
+    stackedWidget->addWidget(player1v1loc);
+    stackedWidget->addWidget(With_ai);
+
+    stackedWidget->setCurrentWidget(mainmenu);
+
+    setCentralWidget(stackedWidget);
 }
 
 MainWindow::~MainWindow() {}
 
 void MainWindow::main_menu(){
-    QVBoxLayout *main_menu = new QVBoxLayout(mainmenu);
 
-    QLabel *title = new QLabel(this);
+    QLabel *title = new QLabel(mainmenu);
     title->setText("Tower Blaster");
-    title->setAlignment(Qt::AlignHCenter);
+    title->setAlignment(Qt::AlignCenter);
     title->setStyleSheet("font-weight: bold; font-size: 24px;");
-    main_menu->addWidget(title);
+    main_menulay->addWidget(title);
 
-    QPushButton *p1v1 = new QPushButton(this);
+    QPushButton *p1v1 = new QPushButton(mainmenu);
     p1v1->setText("1v1 Player Mode");
-    main_menu->addWidget(p1v1);
+    main_menulay->addWidget(p1v1);
+
+    QPushButton *aibut = new QPushButton(mainmenu);
+    aibut->setText("With computer Mode");
+    main_menulay->addWidget(aibut);
 
     //QMessageBox::about(this, "About Qt Menu Example",
                            //"This is a simple application demonstrating how to create "
                            //"menus, actions, and connect them to slots in Qt Widgets (C++).");
 
     connect(p1v1,SIGNAL(clicked()), this, SLOT(p1v1_click()));
+    connect(aibut,SIGNAL(clicked()), this, SLOT(ai()));
+
+    mainmenu->setLayout(main_menulay);
 }
 
 void MainWindow::player1v1(){
-    QVBoxLayout *master_layout = new QVBoxLayout(player1v1loc);
-    QHBoxLayout *towers = new QHBoxLayout(this);
+    QHBoxLayout *towers = new QHBoxLayout();
 
-    QLabel *toptext = new QLabel(this);
+    QLabel *toptext = new QLabel(player1v1loc);
     toptext->setText("Tower Blaster 1v1");
     toptext->setAlignment(Qt::AlignCenter);
     toptext->setStyleSheet("font-weight: bold; font-size: 16px;");
     master_layout->addWidget(toptext);
 
-    QVBoxLayout *tower1 = new QVBoxLayout(this);
-    QVBoxLayout *tower2 = new QVBoxLayout(this);
+    QVBoxLayout *tower1 = new QVBoxLayout();
+    QVBoxLayout *tower2 = new QVBoxLayout();
     
     drawn_card = deck.draw_rand_card();
 
@@ -81,11 +95,13 @@ void MainWindow::player1v1(){
     drawn_card_label->setAlignment(Qt::AlignCenter);
     drawn_card_label->setStyleSheet("font-weight: bold; font-size: 14px;");
 
-    QPushButton *newcard = new QPushButton(this);
     newcard->setText("Draw New Card");
+    discardcard->setText("Discard move");
+    player_move->setText("Player 1's Move");
+    player_move->setAlignment(Qt::AlignCenter);
 
     for (int i = 0; i < 10; ++i) {
-        QPushButton *button1 = new QPushButton(this);
+        QPushButton *button1 = new QPushButton(player1v1loc);
         button1->setText(to_string(deck.draw_rand_card()).c_str());
         buttons1[i]=button1;
         tower1->addWidget(button1);
@@ -93,26 +109,41 @@ void MainWindow::player1v1(){
 
         connect(button1,SIGNAL(clicked()), this, SLOT(On_tower_clicked()));
 
-        QPushButton *button2 = new QPushButton(this);
+        QPushButton *button2 = new QPushButton(player1v1loc);
         button2->setText(to_string(deck.draw_rand_card()).c_str());
         buttons2[i]=button2;
         tower2->addWidget(button2);
+        button2->setEnabled(false);
         button2->setProperty("button_index", i);
 
         connect(button2,SIGNAL(clicked()), this, SLOT(On_tower_clicked()));
     }
+    restart->setText("Restart Game");
+    menubutton->setText("Main Menu");
 
     connect(newcard, SIGNAL(clicked()), this, SLOT(On_draw_clicked()));
-
+    connect(discardcard, SIGNAL(clicked()), this, SLOT(On_discard_clicked()));
+    connect(restart, SIGNAL(clicked()), this, SLOT(On_restart_clicked()));
+    connect(menubutton, SIGNAL(clicked()), this, SLOT(On_mainmenu_clicked()));
+    
     towers->addLayout(tower1);
     towers->addLayout(tower2);
 
     master_layout->addLayout(towers);
+    master_layout->addWidget(player_move);
     master_layout->addWidget(drawn_card_label);
     master_layout->addWidget(newcard);
+    master_layout->addWidget(discardcard);
+    master_layout->addWidget(restart);
+    master_layout->addWidget(menubutton);
 
     player1v1loc->setLayout(master_layout);
+}
 
+void MainWindow::with_ai(){
+    
+
+    With_ai->setLayout(master_ailayout);
 }
 
 //Here are the slot implementations
@@ -120,7 +151,24 @@ void MainWindow::On_tower_clicked() {
     QPushButton *button = qobject_cast<QPushButton*>(sender());
 
     int button_index = button->property("button_index").toInt();
+
+    newcard->setEnabled(true);
+
+    for(int i = 0; i < 10; i++) {
+        if(flipflop) {
+            buttons1[i]->setEnabled(false);
+            buttons2[i]->setEnabled(true);
+            player_move->setText("Player 2's Move");
+        }
+        if(!flipflop) {
+            buttons2[i]->setEnabled(false);
+            buttons1[i]->setEnabled(true);
+            player_move->setText("Player 1's Move");
+        }
+    }
      
+    flipflop = !flipflop;
+
     int temp = drawn_card;
     drawn_card = button->text().toInt();
     button->setText(QString::number(temp));
@@ -129,8 +177,19 @@ void MainWindow::On_tower_clicked() {
 
     if (checkwin(buttons1)) {
         drawn_card_label->setText("Player 1 Wins!");
-    } else if (checkwin(buttons2)) {
+        for(int i = 0; i < 10; i++) {
+            buttons1[i]->setEnabled(false);
+            buttons2[i]->setEnabled(false);
+            discardcard->setEnabled(false);
+        }
+    } 
+    else if (checkwin(buttons2)) {
         drawn_card_label->setText("Player 2 Wins!");
+        for(int i = 0; i < 10; i++) {
+            buttons1[i]->setEnabled(false);
+            buttons2[i]->setEnabled(false);
+            discardcard->setEnabled(false);
+        }
     }
 }
 
@@ -138,8 +197,56 @@ void MainWindow::On_draw_clicked() {
     deck.discard_card(drawn_card);
     drawn_card = deck.draw_rand_card();
     drawn_card_label->setText(("Drawn Card: " + to_string(drawn_card)).c_str());
+    newcard->setEnabled(false);
+}
+
+void MainWindow::On_discard_clicked() {
+    newcard->setEnabled(true);
+
+    for(int i = 0; i < 10; i++) {
+        if(flipflop) {
+            player_move->setText("Player 2's Move");
+            buttons1[i]->setEnabled(false);
+            buttons2[i]->setEnabled(true);
+        }
+        if(!flipflop) {
+            player_move->setText("Player 1's Move");
+            buttons2[i]->setEnabled(false);
+            buttons1[i]->setEnabled(true);
+        }
+    }
+     
+    flipflop = !flipflop;
+}
+
+void MainWindow::On_restart_clicked() {
+    deck.refill();
+    drawn_card = deck.draw_rand_card();
+    drawn_card_label->setText(("Drawn Card: " + to_string(drawn_card)).c_str());
+    player_move->setText("Player 1's Move");
+    newcard->setEnabled(true);
+    discardcard->setEnabled(true);
+
+    for (int i = 0; i < 10; ++i) {
+        buttons1[i]->setText(to_string(deck.draw_rand_card()).c_str());
+        buttons1[i]->setEnabled(true);
+        buttons2[i]->setText(to_string(deck.draw_rand_card()).c_str());
+        buttons2[i]->setEnabled(false);
+    }
+
+    flipflop = true;
+}
+
+void MainWindow::On_mainmenu_clicked() {
+    On_restart_clicked();
+    stackedWidget->setCurrentWidget(mainmenu);
+}  
+
+void MainWindow::ai() {
+    stackedWidget->setCurrentWidget(With_ai);
+    QMessageBox::information(this, "Info", "AI Mode is under construction.");
 }
 
 void MainWindow::p1v1_click() {
-    setCentralWidget(player1v1loc);
+    stackedWidget->setCurrentWidget(player1v1loc);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,13 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include<QMainWindow>
 
 #include<QWidget>
 #include<QPushButton>
 #include<QLabel>
 #include<QVBoxLayout>
 #include<QHBoxLayout>
+#include<QStackedWidget>
 
 #include<iostream>
 #include<limits>
@@ -40,10 +41,6 @@ class deck{
         }
 
         int draw_rand_card(){
-            if(cards.empty()){
-                cout << "Deck is empty!\n";
-                return -1;
-            }
             int rand_card_index = randomInt(0, cards.size()-1);
             int card = cards[rand_card_index];
             cards.erase(cards.begin() + rand_card_index);
@@ -78,24 +75,44 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    QWidget *mainmenu = new QWidget(this);
-    QWidget *player1v1loc = new QWidget(this);
-
+   
     deck deck; 
-    int drawn_card; 
-    QLabel* drawn_card_label= new QLabel(this); 
-    
+    int drawn_card;
+    bool flipflop = true;
+
     std::array<QPushButton*, 10> buttons1;
     std::array<QPushButton*, 10> buttons2;
 
+    QStackedWidget *stackedWidget = new QStackedWidget(this);
+
+    QLabel* drawn_card_label= new QLabel(this); 
+    QLabel* player_move= new QLabel(this); 
+    QPushButton *newcard = new QPushButton(this);
+    QPushButton *discardcard = new QPushButton(this);
+    QPushButton *restart = new QPushButton(this);
+    QPushButton *menubutton = new QPushButton(this);
+
+    QWidget *mainmenu = new QWidget(this);
+    QVBoxLayout *main_menulay = new QVBoxLayout(mainmenu);
+
+    QWidget *player1v1loc = new QWidget(this);
+    QVBoxLayout *master_layout = new QVBoxLayout(player1v1loc);
+
+    QWidget *With_ai = new QWidget(this);
+    QVBoxLayout *master_ailayout = new QVBoxLayout(With_ai);
+
     void player1v1();
     void main_menu();
+    void with_ai();
 
 public slots:
     void On_tower_clicked();
     void On_draw_clicked();
     void p1v1_click();
-    //void On_discard_clicked();
+    void ai();
+    void On_discard_clicked();
+    void On_restart_clicked();
+    void On_mainmenu_clicked();
 };
 
 #endif // MAINWINDOW_H
